9a7ee9512e128e26a0bd43a38e30ab2fd8b3b1a1
gforce81 <g@gforce.io>
2017-06-28 09:07:19 -0400

    initial commit

---
+++ url-parser.py
@@ -0,0 +1,20 @@
+from urlparse import urlparse
+
+# captures the Web Path
+path = urlparse(document_api.v1.get_meta_data_value("originaluri")[0]).path
+# Set the category
+try:
+    category1 = "webroot" if path in ("/", "", None) else path.split("/")[1]
+    document_api.v1.add_meta_data({'meta1': category1})
+except:
+    print("error on category1")
+try:
+    category2 = "webroot" if path in ("/", "", None) else path.split("/")[2]
+    document_api.v1.add_meta_data({'meta2': category2})
+except:
+    print("error on category2")
+try:
+    category3 = "webroot" if path in ("/", "", None) else path.split("/")[3]
+    document_api.v1.add_meta_data({'meta3': category3})
+except:
+    print("error on category3")
